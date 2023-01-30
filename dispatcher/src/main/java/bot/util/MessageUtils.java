6c4ceafc1447e6bfc0ae0d16beedac9a03eaf0b6
Alex <suharevlmgt@gmail.com>
2023-01-30 15:06:20 +0300

    3

---
+++ dispatcher/src/main/java/bot/util/MessageUtils.java
@@ -0,0 +1,19 @@
+package bot.util;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import javax.inject.Singleton;
+
+@Component
+public class MessageUtils {
+
+    public SendMessage generateSendMessage(Update update, String text) {
+        var message = update.getMessage();
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText(text);
+        return sendMessage;
+    }
+}
